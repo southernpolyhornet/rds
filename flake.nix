ca3103f62568db3b6c89043ed4044d02228ade9d
morgan <southernpolyhornet@protonmail.com>
2026-02-21 20:54:11 -0600

    debugging

--- flake.nix
+++ flake.nix
@@ -3,24 +3,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-  in {
+  outputs = { self, nixpkgs }: {
     nixosModules.rds = import ./nix/rds.nix;
-    nixosConfigurations.minimal = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        self.nixosModules.rds
-        {
-          services.rds.enable = true;
-          services.rds.engines.postgres.enable = true;
-          # Minimal config required for nixosSystem evaluation.
-          fileSystems."/".device = "/dev/null";
-          boot.loader.grub.devices = [ "/dev/null" ];
-          system.stateVersion = "24.11";
-        }
-      ];
-    };
-    checks.${system}.minimal = self.nixosConfigurations.minimal.config.system.build.toplevel;
   };
 }
